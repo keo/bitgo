74f84c55c51399ccf8475193114c7368f5e90fe3
Tristan Peralta <tristanperalta@gmail.com>
2015-06-02 16:11:49 +0800

    Add Wallet#create

--- spec/wallet_spec.rb
+++ spec/wallet_spec.rb
@@ -3,6 +3,17 @@ require 'spec_helper'
 describe Bitgo::Wallet do
   let(:session) { double(:session) }
 
+  describe '#list' do
+    before do
+      allow(session).to receive(:call) { get_fixture('wallets.json') }
+    end
+
+    it 'returns list of wallets' do
+      wallet = Bitgo::Wallet.new(session)
+      expect(wallet.list).to have_key('wallets')
+    end
+  end
+
   describe '#create' do
     it 'creates wallet' do
       wallet = Bitgo::Wallet.new(session)
