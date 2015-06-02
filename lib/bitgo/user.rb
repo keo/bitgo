96f0e9d338214b1b680b3f11168be7cfc69005b9
Tristan Peralta <tristanperalta@gmail.com>
2015-06-02 18:39:56 +0800

    Bitgo::Use - set instance variables from raw_data

--- lib/bitgo/user.rb
+++ lib/bitgo/user.rb
@@ -1,14 +1,27 @@
 module Bitgo
   class User
-    attr_accessor :session
+    attr_accessor :session, :id, :first_name, :last_name, :full_name, :username
 
-    def initialize(session)
+    def initialize(session, raw_data={})
       @session = session
+      @id            = raw_data['id']
+      name           = raw_data['name'] || {}
+      @first_name    = name['first']
+      @last_name     = name['last']
+      @full_name     = name['full']
+      @username      = raw_data['username']
+      @isActive      = raw_data['isActive']
     end
 
-    def me
+    def active?
+      @isActive
+    end
+
+    def self.me(session)
       request = Net::HTTP::Get.new('/api/v1/user/me')
-      session.call(request)
+      raw_data = session.call(request)
+
+      new(session, raw_data)
     end
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,9 +1,76 @@
 require 'spec_helper'
 
 describe Bitgo::User do
-  describe "#login" do
-    it "returns access token" do
+  let(:session) { double(:session) }
+  let(:user) { Bitgo::User.new(session, data) }
 
+  describe '#id' do
+    let(:data) { { 'id' => 'blah' } }
+
+    it 'returns default id' do
+      expect(Bitgo::User.new(session).id).to be_nil
+    end
+
+    it 'returns value from raw_data' do
+      expect(user.id).to eq data['id']
+    end
+  end
+
+  describe 'name' do
+    let(:data) do
+      { 'name' => { 'first' => 'Juan',
+                    'last'  => 'dela Cruz',
+                    'full'  => 'Juan dela Cruz'} }
+    end
+
+    describe '#first_name' do
+      it 'returns default' do
+        expect(Bitgo::User.new(session).first_name).to be_nil
+      end
+
+      it 'returns value from raw_data' do
+        expect(user.first_name).to eq data['name']['first']
+      end
+    end
+
+    describe '#last_name' do
+      it 'returns default' do
+        expect(Bitgo::User.new(session).last_name).to be_nil
+      end
+
+      it 'returns value from raw_data' do
+        expect(user.last_name).to eq data['name']['last']
+      end
+    end
+
+    describe '#full_name' do
+      it 'returns default' do
+        expect(Bitgo::User.new(session).full_name).to be_nil
+      end
+
+      it 'returns value from raw_data' do
+        expect(user.full_name).to eq data['name']['full']
+      end
+    end
+  end
+
+  describe '#active?' do
+    let(:data) { { 'isActive' => true } }
+    it 'returns default value' do
+      expect(Bitgo::User.new(session).active?).to eq nil
+    end
+
+    it 'returns value from raw_data' do
+      expect(user).to be_active
+    end
+  end
+
+  describe '.me' do
+    before do
+      allow(session).to receive(:call) { get_fixture('user.json') }
+    end
+    it 'returns User object' do
+      expect(Bitgo::User.me(session)).to be_instance_of Bitgo::User
     end
   end
 end
