5755fb0d5ac97280e13504db74319d0cdb40a6ec
Tristan Peralta <tristanperalta@gmail.com>
2015-06-05 22:33:28 +0800

    Mock response with stringyfied json

--- spec/auth_spec.rb
+++ spec/auth_spec.rb
@@ -7,7 +7,9 @@ describe Bitgo::Auth do
       'otp' => '123123123' }
   end
   let(:auth) { Bitgo::Auth.new(options) }
-  let(:success_response) { get_fixture('session.json') }
+  let(:success_response) do
+    double(:response, body: get_fixture('session.json').to_json)
+  end
 
   specify '#encrypted_password' do
     expect(auth.encrypted_password).to eq '8d7bac7712d99ba1c6b40792e856dda926408e1a0c51a8a8dc10cb03a9ba6d2b'
